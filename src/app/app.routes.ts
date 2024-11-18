577327a0273a155a6cdff816bff4c66b01014cdd
Fabio Rodrigues <fabio.rod@outlook.pt>
2024-11-18 14:13:51 +0000

    Updates Cesae

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {IndexComponent} from './pages/index/index.component';
+import {GamesComponent} from './pages/game/games.component';
+import {GameDetailsComponent} from './pages/game/details/game-details.component';
+import {GamesListComponents} from './pages/game/list/games-list-components.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: IndexComponent},
+    {path: 'games', component: GamesComponent},
+    {path: 'games/list', component: GamesListComponents},
+    {path: 'games/:id', component: GameDetailsComponent}
+];
